414502653258d17157bddffa7729569e5c076c17
nokchoa <camilo.fifa@hotmail.com>
2018-11-20 05:11:53 -0500

    se completa los primeros 6 controllers en el /controllers/index.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,10 +3,13 @@ const aseguradoras = require('../aseguradoras');
 const citas = require('../citas');
 const departamentos = require('../departamentos');
 const examens = require('../examens');
+const formulas = require('../formulas');
 
 module.exports = {
   acompanantes,
   aseguradoras,
   citas,
   departamentos,
+  examens,
+  formulas
 };
