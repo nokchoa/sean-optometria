b76b1cfc508357d221410baf6e13662bafc6fd52
nokchoa <camilo.fifa@hotmail.com>
2018-11-19 19:17:25 -0500

    Borro modelos, migrations, rutas y etc de prueba

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,5 @@
-const Animal = require('./animal');
-const Specie = require('./specie');
-const Class = require('./class');
-const Table = require('./table');
+
 
 module.exports = {
-    Animal,
-    Specie,
-    Class,
-    Table
+    
 };
--- server/routes/table.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Table = require('../controllers').Table;
-
-router.delete('/table', Table.purge);
-
-module.exports = router;
